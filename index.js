30c33deb91bb7a84a9bbcd4cd7a06ce7d91fca67
levibarra <157933526+levibarra@users.noreply.github.com>
2024-03-19 11:03:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //window.confirm('sexo de ladinho?')
 const nomeAventureiro = prompt('Qual seu nome, aventureiro(a)?')
 
-if (confirm("Seja bem vindo(a) " + nomeAventureiro + '! Gostaria de pagar R$50,00 para passar a noite na nossa hospedagem?')) {
+if (confirm("Seja bem vinde " + nomeAventureiro + '! Gostaria de pagar R$50,00 para passar a noite na nossa hospedagem?')) {
     alert("Ótimo! Nós temos as melhores camas de toda a região!");
   } else {
     alert("Que pena! Você parecia ser uma pessoa mais legal...");
